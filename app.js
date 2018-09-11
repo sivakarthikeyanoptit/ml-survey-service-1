ca77f386df1a6db727ea2e11e4a18d0d2176d9fd
yogesh <yogesh.sinoriya@above-inc.com>
2018-09-11 10:47:00 +0530

    [UPDATE] base url 'assessment' added in routes

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
+var express = require("express");
+var routes = require("./routes");
+var http = require("http");
+var path = require("path");
 
 //load customers route
-var schools = require('./routes/schools');
+var schools = require("./routes/schools");
 //load cassandra route
-var cassandrainfo = require('./routes/cassandrainfo');
+var cassandrainfo = require("./routes/cassandrainfo");
 
 var app = express();
 
-var cassandra = require('cassandra-driver');
+var cassandra = require("cassandra-driver");
 
 const client = new cassandra.Client({ contactPoints: ["10.160.0.8"] });
 
 // all environments
-app.set('port', process.env.PORT || 4201);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.logger('dev'));
+app.set("port", process.env.PORT || 4201);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', routes.index);
-app.get('/cassandrainfo', cassandrainfo.init_cassandra);
-app.get('/schools', schools.list);
-
+app.get("/assessment/", routes.index);
+app.get("/assessment/cassandrainfo", cassandrainfo.init_cassandra);
+app.get("/assessment/schools", schools.list);
 
 app.use(app.router);
 
-http.createServer(app).listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function() {
+  console.log("Express server listening on port " + app.get("port"));
 });
