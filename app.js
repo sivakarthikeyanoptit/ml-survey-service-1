22d3fab0a6715f63c30beea5840f2a15c91ddace
yogesh <yogesh.sinoriya@above-inc.com>
2018-09-12 15:18:10 +0530

    [ADD] .gitignore file added, package-lock.json & and node_modules added in ignore list

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-let config = require('./config');
-let router = require('./routes');
+let config = require("./config");
+let router = require("./routes");
 
-const requireAll = require('require-all');
-var compression = require('compression')
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-let app = require('express')();
+const requireAll = require("require-all");
+var compression = require("compression");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+let app = require("express")();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/ping', (req, res) => {
-    res.send("pong!");
+app.get("/ping", (req, res) => {
+  res.send("pong!");
 });
 
 // boostrap all models
 global.models = requireAll({
-    dirname: __dirname + '/models',
-    filter: /(.+)\.js$/,
-    resolve: function (Model) {
-        return Model;
-    }
+  dirname: __dirname + "/models",
+  filter: /(.+)\.js$/,
+  resolve: function(Model) {
+    return Model;
+  }
 });
 
 // boostrap all controllers
 global.controllers = requireAll({
-    dirname: __dirname + '/controllers',
-    filter: /(.+Controller)\.js$/,
-    resolve: function (Controller) {
-        return new Controller(models[Controller.name]);
-    }
+  dirname: __dirname + "/controllers",
+  filter: /(.+Controller)\.js$/,
+  resolve: function(Controller) {
+    return new Controller(models[Controller.name]);
+  }
 });
 
-app.use(morgan('dev'));
-app.all('*', (req, res, next) => {
-    console.log('---------------------------------------------------------------------------');
-    console.log('%s %s on %s from ', req.method, req.url, new Date(), req.headers['user-agent']);
-    console.log('Request Headers: ', req.headers);
-    console.log('Request Body: ', req.body);
-    console.log('Request Files: ', req.files);
-    next();
+app.use(morgan("dev"));
+app.all("*", (req, res, next) => {
+  console.log(
+    "---------------------------------------------------------------------------"
+  );
+  console.log(
+    "%s %s on %s from ",
+    req.method,
+    req.url,
+    new Date(),
+    req.headers["user-agent"]
+  );
+  console.log("Request Headers: ", req.headers);
+  console.log("Request Body: ", req.body);
+  console.log("Request Files: ", req.files);
+  next();
 });
 
 router(app);
 
 app.listen(config.port, () => {
-    console.log("Application is running on the port:" + config.port);
+  console.log("Application is running on the port:" + config.port);
 });
--- config/index.js
+++ config/index.js
@@ -8,131 +8,144 @@
  *                    Written under contract by Above Solutions Pvt. Ltd.
  */
 
-
-var installModule = function (config) {
-    global.AbstractController = require('../Generic/AbstractController').init(config);
-}
-
-const configuration = {
-    development: {
-        root: require('path').normalize(__dirname + '/..'),
-        app: {
-            name: 'doxtro-api'
-        },
-        host: process.env.HOST || 'http://localhost',
-        port: process.env.PORT || 8020,
-        DB_Config: {
-            connection: {
-                mongodb: {
-                    host: process.env.MONGODB_URL || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017",
-                    user: "",
-                    pass: "",
-                    database: process.env.DB || 'Project-Name-Development'
-                }
-            },
-            cassandraConnection: {
-                clientOptions: {
-                    contactPoints: ['35.200.172.221'],
-                    // protocolOptions: { port: 9042 },
-                    keyspace: 'shikshalokam',
-                    user: "",
-                    pass: ""
-                },
-                ormOptions: {
-                    defaultReplicationStrategy: {
-                        class: 'SimpleStrategy',
-                        replication_factor: 1
-                    },
-                    migration: 'safe',
-                }
-            }
-        },
-        version: '0.0.1',
-        URLPrefix: '/api/v1',
-        security: {
-            tokenLife: 3600
-        },
-        email: {
-            senderEmail: '',
-            password: ''
-        },
-        LoginByPass: false
-    },
-    staging: {
-        root: require('path').normalize(__dirname + '/..'),
-        app: {
-            name: 'doxtro-api'
-        },
-        host: process.env.HOST || 'http://localhost',
-        port: process.env.PORT || 8001,
-        DB_Config: {
-            connection: {
-                mongodb: {
-                    host: process.env.MONGODB_URL || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017",
-                    user: "",
-                    pass: "",
-                    database: process.env.DB || 'Project-Name-Staging'
-                }
-            }
-        },
-        version: '0.0.1',
-        URLPrefix: '/api/v1',
-        security: {
-            tokenLife: 3600
-        },
-        facebook: {
-            clientID: '',
-            clientSecret: ''
-        },
-        google: {
-            clientID: '',
-            clientSecret: ''
-        },
-        email: {
-            senderEmail: '',
-            password: ''
-        },
-        LoginByPass: false
-    },
-    testing: {
-        root: require('path').normalize(__dirname + '/..'),
-        app: {
-            name: 'doxtro-api'
-        },
-        host: process.env.HOST || 'http://localhost',
-        port: process.env.PORT || 8000,
-        DB_Config: {
-            connection: {
-                mongodb: {
-                    host: process.env.MONGODB_URL || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017",
-                    user: "",
-                    pass: "",
-                    database: process.env.DB || 'Project-Name-Testing'
-                }
-            }
-        },
-        version: '0.0.1',
-        URLPrefix: '/api/v1',
-        security: {
-            tokenLife: 3600
-        },
-        facebook: {
-            clientID: '',
-            clientSecret: ''
-        },
-        google: {
-            clientID: '',
-            clientSecret: ''
-        },
-        email: {
-            senderEmail: '',
-            password: ''
-        },
-        LoginByPass: false
-    }
+var installModule = function(config) {
+  global.AbstractController = require("../Generic/AbstractController").init(
+    config
+  );
 };
 
-let env = process.env.NODE_ENV || 'development';
+const configuration = {
+  development: {
+    root: require("path").normalize(__dirname + "/.."),
+    app: {
+      name: "doxtro-api"
+    },
+    host: process.env.HOST || "http://localhost",
+    port: process.env.PORT || 3000,
+    DB_Config: {
+      connection: {
+        mongodb: {
+          host:
+            process.env.MONGODB_URL ||
+            process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            "mongodb://localhost:27017",
+          user: "",
+          pass: "",
+          database: process.env.DB || "Project-Name-Development"
+        }
+      },
+      cassandraConnection: {
+        clientOptions: {
+          contactPoints: ["35.200.172.221"],
+          // protocolOptions: { port: 9042 },
+          keyspace: "shikshalokam",
+          user: "",
+          pass: ""
+        },
+        ormOptions: {
+          defaultReplicationStrategy: {
+            class: "SimpleStrategy",
+            replication_factor: 1
+          },
+          migration: "safe"
+        }
+      }
+    },
+    version: "0.0.1",
+    URLPrefix: "/api/v1",
+    security: {
+      tokenLife: 3600
+    },
+    email: {
+      senderEmail: "",
+      password: ""
+    },
+    LoginByPass: false
+  },
+  staging: {
+    root: require("path").normalize(__dirname + "/.."),
+    app: {
+      name: "doxtro-api"
+    },
+    host: process.env.HOST || "http://localhost",
+    port: process.env.PORT || 8001,
+    DB_Config: {
+      connection: {
+        mongodb: {
+          host:
+            process.env.MONGODB_URL ||
+            process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            "mongodb://localhost:27017",
+          user: "",
+          pass: "",
+          database: process.env.DB || "Project-Name-Staging"
+        }
+      }
+    },
+    version: "0.0.1",
+    URLPrefix: "/api/v1",
+    security: {
+      tokenLife: 3600
+    },
+    facebook: {
+      clientID: "",
+      clientSecret: ""
+    },
+    google: {
+      clientID: "",
+      clientSecret: ""
+    },
+    email: {
+      senderEmail: "",
+      password: ""
+    },
+    LoginByPass: false
+  },
+  testing: {
+    root: require("path").normalize(__dirname + "/.."),
+    app: {
+      name: "doxtro-api"
+    },
+    host: process.env.HOST || "http://localhost",
+    port: process.env.PORT || 8000,
+    DB_Config: {
+      connection: {
+        mongodb: {
+          host:
+            process.env.MONGODB_URL ||
+            process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            "mongodb://localhost:27017",
+          user: "",
+          pass: "",
+          database: process.env.DB || "Project-Name-Testing"
+        }
+      }
+    },
+    version: "0.0.1",
+    URLPrefix: "/api/v1",
+    security: {
+      tokenLife: 3600
+    },
+    facebook: {
+      clientID: "",
+      clientSecret: ""
+    },
+    google: {
+      clientID: "",
+      clientSecret: ""
+    },
+    email: {
+      senderEmail: "",
+      password: ""
+    },
+    LoginByPass: false
+  }
+};
+
+let env = process.env.NODE_ENV || "development";
 installModule(configuration[env]);
 
 module.exports = configuration[env];
