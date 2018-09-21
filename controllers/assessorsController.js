5ef81d5743d3c9ce5d4a073759a4e4333f16ff61
Akash Shah <akash.shah@above-inc.com>
2018-09-21 14:37:17 +0530

    Fix response for school assessors.

--- controllers/assessorsController.js
+++ controllers/assessorsController.js
@@ -1,9 +1,39 @@
 module.exports = class Assessors {
+  // async schools(req) {
+  //   req.query = { userId: req.userDetails.userId };
+  //   req.populate = "schools";
+  //   let result = await controllers.schoolAssessorsController.populate(req);
+  //   result.data = await result.data;
+  //   return result;
+  // }
+
   async schools(req) {
-    req.query = { userId: req.userDetails.userId };
-    req.populate = "schools";
-    let result = await controllers.schoolAssessorsController.populate(req);
-    result.data = await result.data;
-    return result;
+    return new Promise(async (resolve,reject) => {
+
+      req.query = { userId: req.userDetails.userId };
+      req.populate = "schools";
+      const queryResult = await super.populate(req)
+      let schools = []
+      queryResult.result.forEach(assessor => {
+        assessor.schools.forEach(assessorSchool => {
+          let schoolInfo = {
+            "_id":assessorSchool._id
+          }
+          console.log(assessorSchool)
+          assessorSchool.form.forEach(formField => {
+            if(formField.field === 'externalId') {
+              schoolInfo.externalId = formField.value
+            }
+            if(formField.field === 'name') {
+              schoolInfo.name = formField.value
+            }
+          })
+          schools.push(schoolInfo)
+        })
+      });
+      return resolve({
+        message: schools
+      });
+    })
   }
 };
