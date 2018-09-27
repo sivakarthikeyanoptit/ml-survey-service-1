00362ed99232c4a4462501457e680671591be583
Akash Shah <akash.shah@above-inc.com>
2018-09-27 10:40:27 +0530

    Fix school assessors API

--- controllers/assessorsController.js
+++ controllers/assessorsController.js
@@ -12,25 +12,26 @@ module.exports = class Assessors {
 
       req.query = { userId: req.userDetails.userId };
       req.populate = "schools";
-      const queryResult = await controllers.schoolAssessorsController.populate(req)
-      let schools = []
-      queryResult.result.forEach(assessor => {
-        assessor.schools.forEach(assessorSchool => {
-          let schoolInfo = {
-            "_id":assessorSchool._id
-          }
-          console.log(assessorSchool)
-          assessorSchool.form.forEach(formField => {
-            if(formField.field === 'externalId') {
-              schoolInfo.externalId = formField.value
-            }
-            if(formField.field === 'name') {
-              schoolInfo.name = formField.value
-            }
-          })
-          schools.push(schoolInfo)
-        })
-      });
+      const schools = await controllers.schoolAssessorsController.populate(req)
+      //let schools = []
+      //console.log(schools)
+      // queryResult.result.forEach(assessor => {
+      //   assessor.schools.forEach(assessorSchool => {
+      //     let schoolInfo = {
+      //       "_id":assessorSchool._id
+      //     }
+      //     console.log(assessorSchool)
+      //     assessorSchool.form.forEach(formField => {
+      //       if(formField.field === 'externalId') {
+      //         schoolInfo.externalId = formField.value
+      //       }
+      //       if(formField.field === 'name') {
+      //         schoolInfo.name = formField.value
+      //       }
+      //     })
+      //     schools.push(schoolInfo)
+      //   })
+      // });
       return resolve({
         message: "School list fetched successfully",
         result:schools
