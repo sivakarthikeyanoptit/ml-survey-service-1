60e6464112205dc910f2495abb8198d07e771fd9
Akash Shah <akash.shah@above-inc.com>
2018-10-25 17:28:24 +0530

    Increase body parser memory limit

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ global.controllers = requireAll({
 });
 
 app.use(fileUpload());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 app.use(express.static("public"));
 
 //request logger
