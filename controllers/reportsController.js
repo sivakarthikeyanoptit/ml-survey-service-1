3c2662c18df85650ae7a4af0bd0c252e53ebcd11
Akash Shah <akash.shah@above-inc.com>
2018-12-01 15:41:57 +0530

    School wise assessors and assessor school mapping API.

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -170,6 +170,414 @@ module.exports = class Reports extends Abstract {
         return resolve({
           data:csv,
           csvResponse:true,
+          fileName:"schoolWiseSubmissionReport "+ new Date().toDateString() +".csv"
+        });
+
+      } catch (error) {
+        return reject({
+          status:500,
+          message:"Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
+
+
+  async assessorSchools(req) {
+    return new Promise(async (resolve, reject) => {
+
+      try {
+        
+        req.query = {};
+        req.populate = {
+          path: 'schools',
+          select: ["name","externalId"]
+        };
+        const assessorsWithSchoolDetails = await controllers.schoolAssessorsController.populate(req)
+
+        let assessorSchools = new Array
+        assessorsWithSchoolDetails.result.forEach(assessor => {
+          assessor.schools.forEach(assessorSchool => {
+            assessorSchools.push({
+              id:assessor.externalId,
+              userId:assessor.userId,
+              parentId:assessor.parentId,
+              name:assessor.name,
+              email:assessor.email,
+              role:assessor.role,
+              programId:assessor.programId.toString(),
+              schoolId:assessorSchool.externalId,
+              schoolName:assessorSchool.name,
+            })
+          })
+        })
+
+        let responseMessage = "Assessor information fetched successfully."
+
+        let response = { result: assessorSchools,message: responseMessage};
+
+        return resolve(response)
+
+        let currentResult = new Array();
+        let submissions = await database.models.submissions.find({});
+        
+        submissions.forEach(submission => {
+
+          let res = new Array();
+          let result = {};
+
+          if(submission.schoolInformation) {
+            result.schoolId = submission.schoolInformation.externalId;
+            result.schoolName = submission.schoolInformation.name;
+          } else {
+            result.schoolId = submission.schoolId;
+          }
+
+
+          if(submission.programInformation) {
+            result.programId = submission.programId;
+            result.programName = submission.programInformation.name;
+          } else {
+            result.programId = submission.programId;
+          }
+
+          result.status = submission.status;
+
+          let evidenceMethodStatuses = Object.entries(submission.evidences).map(evidenceMethod => ({
+            [evidenceMethod[0]]: evidenceMethod[1].isSubmitted
+          }));
+          evidenceMethodStatuses.forEach(evidenceMethodStatus => {
+            _.merge(result, evidenceMethodStatus);
+          });
+
+          let hasConflicts = Object.entries(submission.evidences).map(evidenceMethod => ({
+            [evidenceMethod[1].name]: evidenceMethod[1].hasConflicts
+          }));
+          hasConflicts.forEach(hasConflictsObject => {
+            _.merge(result, hasConflictsObject);
+          });
+
+          res.push(result);
+          res.forEach(individualResult => {
+            currentResult.push(individualResult);
+          });
+
+        });
+
+
+        const fields = [
+          {
+            label: "Program Id",
+            value: "programId"
+          },
+          {
+            label: "Program Name",
+            value: "programName"
+          },
+          {
+            label: "School Id",
+            value: "schoolId"
+          },
+          {
+            label: "School Name",
+            value: "schoolName"
+          },
+          {
+            label: "School status",
+            value: "status"
+          },
+          {
+            label: "BL",
+            value: "BL"
+          },
+          {
+            label: "BL-dup",
+            value: "Book Look"
+          },
+          {
+            label: "LW",
+            value: "LW"
+          },
+          {
+            label: "LW-dup",
+            value: "Learning Walk"
+          },
+          {
+            label: "SI",
+            value: "SI"
+          },
+          {
+            label: "SI-dup",
+            value: "Student Interview"
+          },
+          {
+            label: "AC3",
+            value: "AC3"
+          },
+          {
+            label: "AC3-dup",
+            value: "Assessment- Class 3"
+          },
+          {
+            label: "AC5",
+            value: "AC5"
+          },
+          {
+            label: "AC5",
+            value: "Assessment- Class 5"
+          },
+          {
+            label: "AC8",
+            value: "AC8"
+          },
+          {
+            label: "AC8-dup",
+            value: "Assessment- Class 8"
+          },
+          {
+            label: "PI",
+            value: "PI"
+          },
+          {
+            label: "PI-dup",
+            value: "Principal Interview"
+          },
+          {
+            label: "PAI",
+            value: "PAI"
+          },
+          {
+            label: "PAI-dup",
+            value: "Parent Interview"
+          },
+          {
+            label: "CO",
+            value: "CO"
+          },
+          {
+            label: "CO-dup",
+            value: "Classroom Observation"
+          },
+          {
+            label: "TI",
+            value: "TI"
+          },
+          {
+            label: "TI-dup",
+            value: "Teacher Interview"
+          }
+        ];
+        
+        const json2csvParser = new json2csv({ fields });
+        const csv = json2csvParser.parse(currentResult);
+        return resolve({
+          data:csv,
+          csvResponse:true,
+          fileName:"schoolWiseSubmissionReport"+new Date().toDateString()+".csv"
+        });
+
+      } catch (error) {
+        return reject({
+          status:500,
+          message:"Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
+
+
+  async schoolAssessors(req) {
+    return new Promise(async (resolve, reject) => {
+
+      try {
+        
+        req.query = {};
+        req.populate = {
+          path: 'schools',
+          select: ["name","externalId"]
+        };
+        const assessorsWithSchoolDetails = await controllers.schoolAssessorsController.populate(req)
+
+        let schoolAssessors = new Array
+        assessorsWithSchoolDetails.result.forEach(assessor => {
+          assessor.schools.forEach(assessorSchool => {
+            schoolAssessors.push({
+              id:assessorSchool.externalId,
+              name:assessorSchool.name,
+              assessorUserId:assessor.userId,
+              assessorId:assessor.externalId,
+              assessorName:assessor.name,
+              assessorEmail:assessor.email,
+              assessorParentId:assessor.parentId,
+              assessorRole:assessor.role,
+              programId:assessor.programId.toString(),
+            })
+          })
+        })
+
+        let responseMessage = "Assessor information fetched successfully."
+
+        let response = { result: schoolAssessors,message: responseMessage};
+
+        return resolve(response)
+
+        let currentResult = new Array();
+        let submissions = await database.models.submissions.find({});
+        
+        submissions.forEach(submission => {
+
+          let res = new Array();
+          let result = {};
+
+          if(submission.schoolInformation) {
+            result.schoolId = submission.schoolInformation.externalId;
+            result.schoolName = submission.schoolInformation.name;
+          } else {
+            result.schoolId = submission.schoolId;
+          }
+
+
+          if(submission.programInformation) {
+            result.programId = submission.programId;
+            result.programName = submission.programInformation.name;
+          } else {
+            result.programId = submission.programId;
+          }
+
+          result.status = submission.status;
+
+          let evidenceMethodStatuses = Object.entries(submission.evidences).map(evidenceMethod => ({
+            [evidenceMethod[0]]: evidenceMethod[1].isSubmitted
+          }));
+          evidenceMethodStatuses.forEach(evidenceMethodStatus => {
+            _.merge(result, evidenceMethodStatus);
+          });
+
+          let hasConflicts = Object.entries(submission.evidences).map(evidenceMethod => ({
+            [evidenceMethod[1].name]: evidenceMethod[1].hasConflicts
+          }));
+          hasConflicts.forEach(hasConflictsObject => {
+            _.merge(result, hasConflictsObject);
+          });
+
+          res.push(result);
+          res.forEach(individualResult => {
+            currentResult.push(individualResult);
+          });
+
+        });
+
+
+        const fields = [
+          {
+            label: "Program Id",
+            value: "programId"
+          },
+          {
+            label: "Program Name",
+            value: "programName"
+          },
+          {
+            label: "School Id",
+            value: "schoolId"
+          },
+          {
+            label: "School Name",
+            value: "schoolName"
+          },
+          {
+            label: "School status",
+            value: "status"
+          },
+          {
+            label: "BL",
+            value: "BL"
+          },
+          {
+            label: "BL-dup",
+            value: "Book Look"
+          },
+          {
+            label: "LW",
+            value: "LW"
+          },
+          {
+            label: "LW-dup",
+            value: "Learning Walk"
+          },
+          {
+            label: "SI",
+            value: "SI"
+          },
+          {
+            label: "SI-dup",
+            value: "Student Interview"
+          },
+          {
+            label: "AC3",
+            value: "AC3"
+          },
+          {
+            label: "AC3-dup",
+            value: "Assessment- Class 3"
+          },
+          {
+            label: "AC5",
+            value: "AC5"
+          },
+          {
+            label: "AC5",
+            value: "Assessment- Class 5"
+          },
+          {
+            label: "AC8",
+            value: "AC8"
+          },
+          {
+            label: "AC8-dup",
+            value: "Assessment- Class 8"
+          },
+          {
+            label: "PI",
+            value: "PI"
+          },
+          {
+            label: "PI-dup",
+            value: "Principal Interview"
+          },
+          {
+            label: "PAI",
+            value: "PAI"
+          },
+          {
+            label: "PAI-dup",
+            value: "Parent Interview"
+          },
+          {
+            label: "CO",
+            value: "CO"
+          },
+          {
+            label: "CO-dup",
+            value: "Classroom Observation"
+          },
+          {
+            label: "TI",
+            value: "TI"
+          },
+          {
+            label: "TI-dup",
+            value: "Teacher Interview"
+          }
+        ];
+        
+        const json2csvParser = new json2csv({ fields });
+        const csv = json2csvParser.parse(currentResult);
+        return resolve({
+          data:csv,
+          csvResponse:true,
           fileName:"schoolWiseSubmissionReport"+new Date().toDateString()+".csv"
         });
 
