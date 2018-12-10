12c777ab719454167ec56c6a3d2953bc044fe6d0
Shyam Achuthan <shyam@shyamachuthan.com>
2018-12-10 16:52:27 +0530

    Revert "Graceful handling"
    This reverts commit 984177f674f2f5cb1802302eb157b2ea7f9baf51.

--- controllers/submissionsController.js
+++ controllers/submissionsController.js
@@ -368,26 +368,23 @@ module.exports = class Submission extends Abstract {
 
           let evidenceSubmissionAnswerArray = {}
 
-          if(submissionDocument.parentInterviewResponses){
-              Object.entries(submissionDocument.parentInterviewResponses).forEach(parentInterviewResponse => {
-              if(parentInterviewResponse[1].status === "completed") {
-                Object.entries(parentInterviewResponse[1].answers).forEach(answer => {
-                  if(evidenceSubmissionAnswerArray[answer[0]]) {
-                    answer[1].value.forEach(instanceResponse => {
-                      evidenceSubmissionAnswerArray[answer[0]].value.push(instanceResponse)
-                    })
-                    answer[1].payload.labels[0].forEach(instanceResponsePayload => {
-                      evidenceSubmissionAnswerArray[answer[0]].payload.labels[0].push(instanceResponsePayload)
-                    })
-                    evidenceSubmissionAnswerArray[answer[0]].countOfInstances = evidenceSubmissionAnswerArray[answer[0]].value.length
-                  } else {
-                    evidenceSubmissionAnswerArray[answer[0]] = answer[1]
-                  }
-                })
-              }
-            });
-          }
-          
+          Object.entries(submissionDocument.parentInterviewResponses).forEach(parentInterviewResponse => {
+            if(parentInterviewResponse[1].status === "completed") {
+              Object.entries(parentInterviewResponse[1].answers).forEach(answer => {
+                if(evidenceSubmissionAnswerArray[answer[0]]) {
+                  answer[1].value.forEach(instanceResponse => {
+                    evidenceSubmissionAnswerArray[answer[0]].value.push(instanceResponse)
+                  })
+                  answer[1].payload.labels[0].forEach(instanceResponsePayload => {
+                    evidenceSubmissionAnswerArray[answer[0]].payload.labels[0].push(instanceResponsePayload)
+                  })
+                  evidenceSubmissionAnswerArray[answer[0]].countOfInstances = evidenceSubmissionAnswerArray[answer[0]].value.length
+                } else {
+                  evidenceSubmissionAnswerArray[answer[0]] = answer[1]
+                }
+              })
+            }
+          });
 
           evidenceSubmission.answers = evidenceSubmissionAnswerArray
 
