921d7a4a53bbeaf8ce2d4b5d0f4072c5e8e5936f
aman <aman@tunerlabs.com>
2018-12-29 17:07:32 +0530

    added criterias by schoolId and submission reports by program id

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -12,29 +12,24 @@ module.exports = class Reports extends Abstract {
     return "submissions";
   }
 
-  async dataFix(req){
-     return new Promise(async (resolve, reject) => {
-        try {
+  async dataFix(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let dataFixer = require("../generics/helpers/dataFixer");
+        dataFixer.processData(req.params._id);
 
-          let dataFixer = require("../generics/helpers/dataFixer");
-          dataFixer.processData(req.params._id);
-
-          return resolve({
+        return resolve({
           status: 200,
           message: "All good! for " + req.params._id
-          });
-
-        } catch (error) {
+        });
+      } catch (error) {
         return reject({
           status: 500,
           message: "Oops! Something went wrong!",
           errorObject: error
         });
       }
-
-
-
-     });
+    });
   }
 
   async status(req) {
@@ -506,9 +501,11 @@ module.exports = class Reports extends Abstract {
           data: csv,
           csvResponse: true,
           fileName:
-            " programSchoolsStatus_" + moment(currentDate)
-      .tz("Asia/Kolkata")
-      .format("YYYY_MM_DD_HH_mm") + ".csv"
+            " programSchoolsStatus_" +
+            moment(currentDate)
+              .tz("Asia/Kolkata")
+              .format("YYYY_MM_DD_HH_mm") +
+            ".csv"
         };
         return resolve(response);
       } catch (error) {
@@ -533,13 +530,460 @@ module.exports = class Reports extends Abstract {
           data: csvData,
           csvResponse: true,
           fileName:
-            "ecmWiseReport_" + req.query.evidenceId + "_" +
+            "ecmWiseReport_" +
+            req.query.evidenceId +
+            "_" +
             moment(currentDate)
               .tz("Asia/Kolkata")
               .format("YYYY_MM_DD_HH_mm") +
             ".csv"
         });
+      } catch (error) {
+        return reject({
+          status: 500,
+          message: "Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
 
+  async generateCriteriasBySchoolId(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let schoolId = {
+          ["schoolInformation.externalId"]: req.params._id
+        };
+
+        let submissionDocument = await database.models.submissions.find(
+          schoolId,
+          {
+            criterias: 1
+          }
+        );
+
+        let evaluationFrameworksDocuments = await database.models[
+          "evaluation-frameworks"
+        ].find({});
+
+        let nameData = [];
+        evaluationFrameworksDocuments.forEach(singleDocument => {
+          singleDocument.themes.forEach(singleTheme => {
+            singleTheme.aoi.forEach(singleAoi => {
+              singleAoi.indicators.forEach(singleIndicator => {
+                singleIndicator.criteria.forEach(singleCriteria => {
+                  nameData.push({
+                    themeName: singleTheme.name,
+                    aoiName: singleAoi.name,
+                    indicatorName: singleIndicator.name,
+                    criteriaId: singleCriteria
+                  });
+                });
+              });
+            });
+          });
+        });
+
+        let criteriaReports = [];
+        submissionDocument[0].criterias.forEach(submissionCriterias => {
+          let levels = submissionCriterias.rubric.levels;
+
+          if (submissionCriterias._id) {
+            let nameObject = nameData.find(
+              singleData =>
+                singleData.criteriaId.toString() ==
+                submissionCriterias._id.toString()
+            );
+            criteriaReports.push({
+              themeName: nameObject ? nameObject.themeName : "",
+              aoiName: nameObject ? nameObject.aoiName : "",
+              indicatorName: nameObject ? nameObject.indicatorName : "",
+              criteriaName: submissionCriterias.name,
+              "Level 1": levels.find(level => level.level == "L1").description,
+              "Level 2": levels.find(level => level.level == "L2").description,
+              "Level 3": levels.find(level => level.level == "L3").description,
+              "Level 4": levels.find(level => level.level == "L4").description,
+              score: submissionCriterias.score
+                ? submissionCriterias.score
+                : "NA"
+            });
+          }
+        });
+
+        const fields = [
+          {
+            label: "Theme Name",
+            value: "themeName"
+          },
+          {
+            label: "Aoi Name",
+            value: "aoiName"
+          },
+          {
+            label: "Indicator Name",
+            value: "indicatorName"
+          },
+          {
+            label: "Criteria Name",
+            value: "criteriaName"
+          },
+          {
+            label: "Level 1",
+            value: "Level 1"
+          },
+          {
+            label: "Level 2",
+            value: "Level 2"
+          },
+          {
+            label: "Level 3",
+            value: "Level 3"
+          },
+          {
+            label: "Level 4",
+            value: "Level 4"
+          },
+          {
+            label: "Score",
+            value: "score"
+          }
+        ];
+
+        const json2csvParser = new json2csv({ fields });
+        const csv = json2csvParser.parse(criteriaReports);
+        return resolve({
+          data: csv,
+          csvResponse: true,
+          fileName:
+            "criteriaswiseSchoolReport " + new Date().toDateString() + ".csv"
+        });
+      } catch (error) {
+        return reject({
+          status: 500,
+          message: "Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
+
+  async generateSubmissionReportsByProgramId(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let criteriaQuestionDocument = await database.models.criterias.find(
+          {},
+          { evidences: 1, name: 1 }
+        );
+
+        let criteriaQuestionDetails = [];
+
+        criteriaQuestionDocument.forEach(singleCriteriaQuestion => {
+          singleCriteriaQuestion.evidences.forEach(singleEvidence => {
+            singleEvidence.sections.forEach(singleSection => {
+              singleSection.questions.forEach(singleQuestion => {
+                criteriaQuestionDetails.push({
+                  criteriaName: singleCriteriaQuestion.name,
+                  questionId: singleQuestion
+                });
+              });
+            });
+          });
+        });
+
+        let submissionQuery = {
+          ["programInformation.name"]: req.params._id
+        };
+
+        let queryObject = "evidences." + req.query.evidenceId + "";
+
+        let submissionDocument = await database.models.submissions.find(
+          submissionQuery,
+          {
+            assessors: 1,
+            schoolInformation: 1,
+            programInformation: 1,
+            status: 1,
+            [queryObject]: 1
+          }
+        );
+
+        let assessorElement = {};
+        let ecmReports = [];
+
+        for (
+          let submissionInstance = 0;
+          submissionInstance < submissionDocument.length;
+          submissionInstance++
+        ) {
+          submissionDocument[submissionInstance].assessors.forEach(assessor => {
+            assessorElement[assessor.userId] = {
+              externalId: assessor.externalId
+            };
+          });
+
+          if (
+            submissionDocument[submissionInstance].evidences[
+              req.query.evidenceId
+            ] &&
+            submissionDocument[submissionInstance].evidences[
+              req.query.evidenceId
+            ].isSubmitted &&
+            (submissionDocument[submissionInstance].status == "inprogress" ||
+              submissionDocument[submissionInstance].status == "blocked")
+          ) {
+            submissionDocument[submissionInstance]["evidences"][
+              req.query.evidenceId
+            ].submissions.forEach(submission => {
+              let answer = [];
+
+              if (assessorElement[submission.submittedBy.toString()]) {
+                Object.entries(submission.answers).map(submissionAnswer => {
+                  return answer.push(submissionAnswer[1]);
+                });
+
+                answer.forEach(QAndA => {
+                  let criteriaQuestionObject = criteriaQuestionDetails.find(
+                    singleCriteriaQuestion =>
+                      singleCriteriaQuestion.questionId.toString() ==
+                      QAndA.qid.toString()
+                  );
+
+                  let ecmCurrentReport = [];
+
+                  if (!(QAndA.responseType == "matrix")) {
+                    let imageLink = new Array();
+                    QAndA.fileName.forEach(imageSource => {
+                      let envVar = process.env.NODE_ENV
+                        ? process.env.NODE_ENV
+                        : "development";
+                      let envString = envVar == "production" ? "prod" : "dev";
+
+                      if (imageSource) {
+                        imageLink.push(
+                          " https://storage.cloud.google.com/sl-" +
+                            envString +
+                            "-storage/" +
+                            imageSource.sourcePath +
+                            " "
+                        );
+                      } else {
+                        if (process.env.NODE_ENV == "development") {
+                          console.log("No image link is there");
+                        }
+                      }
+                    });
+
+                    let imagePath;
+
+                    if (imageLink.length) {
+                      imagePath = imageLink.toString();
+                    }
+
+                    QAndA.payload.filesNotUploaded = [];
+
+                    ecmCurrentReport.push({
+                      criteriaName: criteriaQuestionObject.criteriaName,
+                      schoolName:
+                        submissionDocument[submissionInstance].schoolInformation
+                          .name,
+                      schoolId:
+                        submissionDocument[submissionInstance].schoolInformation
+                          .externalId,
+                      question: QAndA.payload["question"][0],
+                      answer: QAndA.payload["labels"].toString(),
+                      assessorId:
+                        assessorElement[submission.submittedBy.toString()]
+                          .externalId,
+                      startTime: this.gmtToIst(QAndA.startTime),
+                      endTime: this.gmtToIst(QAndA.endTime),
+                      image: imagePath
+                    });
+                  } else {
+                    let criteriaQuestionObject = criteriaQuestionDetails.find(
+                      singleCriteriaQuestion =>
+                        singleCriteriaQuestion.questionId.toString() ==
+                        QAndA.qid.toString()
+                    );
+
+                    if (QAndA.payload.filesNotUploaded.length) {
+                      QAndA.payload.filesNotUploaded.splice(
+                        0,
+                        QAndA.payload.filesNotUploaded.length
+                      );
+                    }
+
+                    ecmCurrentReport.push({
+                      criteriaName: criteriaQuestionObject.criteriaName,
+                      schoolName:
+                        submissionDocument[submissionInstance].schoolInformation
+                          .name,
+                      schoolId:
+                        submissionDocument[submissionInstance].schoolInformation
+                          .externalId,
+                      question: QAndA.payload["question"][0],
+                      answer: "Instance Question",
+                      assessorId:
+                        assessorElement[submission.submittedBy.toString()]
+                          .externalId,
+                      startTime: this.gmtToIst(QAndA.startTime),
+                      endTime: this.gmtToIst(QAndA.endTime)
+                    });
+
+                    if (QAndA.payload.labels[0]) {
+                      for (
+                        let instance = 0;
+                        instance < QAndA.payload.labels[0].length;
+                        instance++
+                      ) {
+                        QAndA.payload.labels[0][instance].forEach(
+                          QAndAElement => {
+                            let criteriaQuestionObject = criteriaQuestionDetails.find(
+                              singleCriteriaQuestion =>
+                                singleCriteriaQuestion.questionId.toString() ==
+                                QAndAElement._id.toString()
+                            );
+
+                            let imageLink = new Array();
+                            let envVar = process.env.NODE_ENV
+                              ? process.env.NODE_ENV
+                              : "development";
+                            let envString =
+                              envVar == "production" ? "prod" : "dev";
+
+                            QAndAElement.fileName.forEach(imageSource => {
+                              imageLink.push(
+                                " https://storage.cloud.google.com/sl-" +
+                                  envString +
+                                  "-storage/" +
+                                  submissionDocument[submissionInstance]._id +
+                                  "/" +
+                                  submission.submittedBy.toString() +
+                                  "/" +
+                                  imageSource +
+                                  " "
+                              );
+                            });
+
+                            let imagePath;
+
+                            if (imageLink.length) {
+                              imagePath = imageLink.toString();
+                            }
+
+                            let radioResponse = {};
+                            let multiSelectResponse = {};
+                            let multiSelectResponseArray = [];
+
+                            if (QAndAElement.responseType == "radio") {
+                              QAndAElement.options.forEach(option => {
+                                radioResponse[option.value] = option.label;
+                              });
+                              answer = radioResponse[QAndAElement.value];
+                            } else if (
+                              QAndAElement.responseType == "multiselect"
+                            ) {
+                              QAndAElement.options.forEach(option => {
+                                multiSelectResponse[option.value] =
+                                  option.label;
+                              });
+
+                              QAndAElement.value.forEach(value => {
+                                multiSelectResponseArray.push(
+                                  multiSelectResponse[value]
+                                );
+                              });
+
+                              answer = multiSelectResponseArray.toString();
+                            } else {
+                              answer = QAndAElement.value;
+                            }
+
+                            ecmCurrentReport.push({
+                              criteriaName: criteriaQuestionObject.criteriaName,
+                              schoolName:
+                                submissionDocument[submissionInstance]
+                                  .schoolInformation.name,
+                              schoolId:
+                                submissionDocument[submissionInstance]
+                                  .schoolInformation.externalId,
+                              question: QAndAElement.question[0],
+                              answer: answer,
+                              assessorId:
+                                assessorElement[
+                                  submission.submittedBy.toString()
+                                ].externalId,
+                              startTime: this.gmtToIst(QAndAElement.startTime),
+                              endTime: this.gmtToIst(QAndAElement.endTime),
+                              image: imagePath
+                            });
+                          }
+                        );
+                      }
+                    }
+                  }
+                  ecmCurrentReport.forEach(currentEcm => {
+                    ecmReports.push(currentEcm);
+                  });
+                });
+              }
+            });
+          }
+        }
+
+        let fields = [
+          {
+            label: "Criteria Name",
+            value: "criteriaName"
+          },
+          {
+            label: "School Name",
+            value: "schoolName"
+          },
+          {
+            label: "School Id",
+            value: "schoolId"
+          },
+          {
+            label: "Assessor Id",
+            value: "assessorId"
+          },
+          {
+            label: "Question",
+            value: "question"
+          },
+          {
+            label: "Answers",
+            value: "answer"
+          },
+          {
+            label: "Start Time",
+            value: "startTime"
+          },
+          {
+            label: "End Time",
+            value: "endTime"
+          },
+          {
+            label: "Image",
+            value: "image"
+          }
+        ];
+
+        const json2csvParser = new json2csv({ fields });
+        const csv = json2csvParser.parse(ecmReports);
+        let currentDate = new Date();
+        return resolve({
+          data: csv,
+          csvResponse: true,
+          fileName:
+            "submissionReportByProgramId" +
+            req.query.evidenceId +
+            "_" +
+            moment(currentDate)
+              .tz("Asia/Kolkata")
+              .format("YYYY_MM_DD_HH_mm") +
+            ".csv"
+        });
       } catch (error) {
         return reject({
           status: 500,
