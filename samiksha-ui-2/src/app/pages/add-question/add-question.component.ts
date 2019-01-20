fb2a1aa4ee43a75df1ac3f420b2a206868df9a80
Akash Shah <akash.shah@above-inc.com>
2019-01-20 22:46:50 +0530

    Remove sections which are not required for m100.

--- samiksha-ui-2/src/app/pages/add-question/add-question.component.ts
+++ samiksha-ui-2/src/app/pages/add-question/add-question.component.ts
@@ -364,11 +364,15 @@ export class AddQuestionComponent implements OnInit {
       }
     ];
 
+    // this.DCPCRsections = [
+    //   { name: "Survey Questions", questions: [] },
+    //   { name: "Data to be Filled", questions: [] },
+    //   { name: "Group Interview", questions: [] },
+    //   { name: "Individual Interview", questions: [] }
+    // ];
+
     this.sections = [
-      { name: "Survey Questions", questions: [] },
-      { name: "Data to be Filled", questions: [] },
-      { name: "Group Interview", questions: [] },
-      { name: "Individual Interview", questions: [] }
+      { name: "Survey Questions", questions: [] }
     ];
   }
 
