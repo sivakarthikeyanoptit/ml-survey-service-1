0fb67851adf0842ace3239d762753dca870ac7df
aman <aman@tunerlabs.com>
2019-01-21 17:13:57 +0530

    added add,remove and update features for assessorsControllers

--- controllers/assessorsController.js
+++ controllers/assessorsController.js
@@ -144,89 +144,89 @@ module.exports = class Assessors {
         }));
 
 
-        // const assessorRoleMapping = {
-        //   ASSESSOR: "assessors",
-        //   LEAD_ASSESSOR: "leadAssessors",
-        //   PROJECT_MANAGER: "projectManagers",
-        //   PROGRAM_MANAGER: "programManagers"
-        // };
+        const assessorRoleMapping = {
+          ASSESSOR: "assessors",
+          LEAD_ASSESSOR: "leadAssessors",
+          PROJECT_MANAGER: "projectManagers",
+          PROGRAM_MANAGER: "programManagers"
+        };
 
-        // if (assessorUploadCount === assessorData.length) {
+        if (assessorUploadCount === assessorData.length) {
 
-        //   let assessorElement = new Object;
-        //   let assessorProgramComponents = new Array
-        //   let indexOfEvaluationFrameworkInProgram
-        //   let programFrameworkRoles = new Array
-        //   let assessorRolePerMap
-        //   let userIdIndexInRole
-        //   let assessorCsvDataProgramId
-        //   let assessorCsvDataEvaluationFrameworkId
+          let assessorElement = new Object;
+          let assessorProgramComponents = new Array
+          let indexOfEvaluationFrameworkInProgram
+          let programFrameworkRoles = new Array
+          let assessorRolePerMap
+          let userIdIndexInRole
+          let assessorCsvDataProgramId
+          let assessorCsvDataEvaluationFrameworkId
 
-        //   for (let assessorIndexInData = 0; assessorIndexInData < assessorData.length; assessorIndexInData++) {
-        //     assessorElement = assessorData[assessorIndexInData];
-        //     assessorCsvDataProgramId = programQueryList[assessorElement.externalId]
-        //     assessorCsvDataEvaluationFrameworkId = evaluationFrameworkQueryList[assessorElement.externalId]
-        //     assessorProgramComponents = programsData[assessorCsvDataProgramId].components
-        //     indexOfEvaluationFrameworkInProgram = assessorProgramComponents.findIndex(component => component.id.toString() === evaluationFrameworksData[assessorCsvDataEvaluationFrameworkId].toString());
+          for (let assessorIndexInData = 0; assessorIndexInData < assessorData.length; assessorIndexInData++) {
+            assessorElement = assessorData[assessorIndexInData];
+            assessorCsvDataProgramId = programQueryList[assessorElement.externalId]
+            assessorCsvDataEvaluationFrameworkId = evaluationFrameworkQueryList[assessorElement.externalId]
+            assessorProgramComponents = programsData[assessorCsvDataProgramId].components
+            indexOfEvaluationFrameworkInProgram = assessorProgramComponents.findIndex(component => component.id.toString() === evaluationFrameworksData[assessorCsvDataEvaluationFrameworkId].toString());
 
-        //     if (indexOfEvaluationFrameworkInProgram >= 0) {
-        //       programFrameworkRoles = assessorProgramComponents[indexOfEvaluationFrameworkInProgram].roles
-        //       assessorRolePerMap = assessorRoleMapping[assessorElement.role]
-        //       Object.keys(programFrameworkRoles).forEach(role => {
-        //         if (role === assessorRolePerMap) {
-        //           if (programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId) < 0) {
-        //             programFrameworkRoles[role].users.push(assessorElement.userId)
-        //           }
-        //         } else if (programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId) > 0) {
-        //           userIdIndexInRole = programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId)
-        //           programFrameworkRoles[role].users.splice(userIdIndexInRole, 1)
-        //         }
-        //       })
-        //     }
+            if (indexOfEvaluationFrameworkInProgram >= 0) {
+              programFrameworkRoles = assessorProgramComponents[indexOfEvaluationFrameworkInProgram].roles
+              assessorRolePerMap = assessorRoleMapping[assessorElement.role]
+              Object.keys(programFrameworkRoles).forEach(role => {
+                if (role === assessorRolePerMap) {
+                  if (programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId) < 0) {
+                    programFrameworkRoles[role].users.push(assessorElement.userId)
+                  }
+                } else if (programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId) > 0) {
+                  userIdIndexInRole = programFrameworkRoles[role].users.findIndex(user => user === assessorElement.userId)
+                  programFrameworkRoles[role].users.splice(userIdIndexInRole, 1)
+                }
+              })
+            }
 
-        //   }
+          }
 
 
-        //   // await Promise.all(assessorData.map(async (assessor) => {
-        //   await Promise.all(Object.values(programsData).map(async (program) => {
-        //     // if (assessor.operation == "Update") {
-        //     let queryObject = {
-        //       _id: ObjectId(program._id.toString())
-        //     }
-        //     let updateObject = {}
+          // await Promise.all(assessorData.map(async (assessor) => {
+          await Promise.all(Object.values(programsData).map(async (program) => {
+            // if (assessor.operation == "Update") {
+            let queryObject = {
+              _id: ObjectId(program._id.toString())
+            }
+            let updateObject = {}
 
-        //     updateObject.$addToSet = {
-        //       ["components"]: program.components
-        //     }
+            updateObject.$addToSet = {
+              ["components"]: program.components
+            }
 
-        //     await database.models.programs.findOneAndUpdate(
-        //       queryObject,
-        //       updateObject
-        //     );
-        //     // }
-        //     // else if (assessor.operation == "Add") {
-        //     //   let queryObject = {
-        //     //     _id: ObjectId(program._id.toString())
-        //     //   }
-        //     //   let updateObject = {}
+            await database.models.programs.findOneAndUpdate(
+              queryObject,
+              updateObject
+            );
+            // }
+            // else if (assessor.operation == "Add") {
+            //   let queryObject = {
+            //     _id: ObjectId(program._id.toString())
+            //   }
+            //   let updateObject = {}
 
-        //     //   updateObject.$addToSet = {
-        //     //     ["components"]: program.components
-        //     //   }
+            //   updateObject.$addToSet = {
+            //     ["components"]: program.components
+            //   }
 
-        //     //   await database.models.programs.findOneAndUpdate(
-        //     //     queryObject,
-        //     //     updateObject
-        //     //   );
-        //     // }
-        //     return
-        //   }));
-        //   // return assessor
-        //   // }))
+            //   await database.models.programs.findOneAndUpdate(
+            //     queryObject,
+            //     updateObject
+            //   );
+            // }
+            return
+          }));
+          // return assessor
+          // }))
 
-        // } else {
-        //   throw "Something went wrong, not all records were inserted/updated."
-        // }
+        } else {
+          throw "Something went wrong, not all records were inserted/updated."
+        }
 
         let responseMessage = "Assessor record created successfully."
 
