953b51a7829f9ca7e44ce51f37f44ae4489c30c5
aman <aman@tunerlabs.com>
2019-02-08 01:31:46 +0530

    added view schools and assessor api

--- controllers/v1/programsController.js
+++ controllers/v1/programsController.js
@@ -82,4 +82,186 @@ module.exports = class Programs extends Abstract {
     return programDocument
   }
 
+  async listSchools(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+
+        let programId = req.query.programId
+
+        if (!programId) {
+          throw "Program id is missing"
+        }
+
+        let componentId = req.query.componentId
+
+        if (!componentId) {
+          throw "Component Id is missing"
+        }
+
+        let programDocument = await database.models.programs.aggregate([
+          {
+            $match: {
+              _id: ObjectId(programId)
+            }
+          }, {
+            $unwind: "$components"
+          }, {
+            $match: {
+              "components.id": ObjectId(componentId)
+            }
+          }, {
+            "$addFields": { "schoolIdInObjectIdForm": "$components.schools" }
+          },
+          {
+            $lookup: {
+              from: "schools",
+              localField: "schoolIdInObjectIdForm",
+              foreignField: "_id",
+              as: "schoolInformation"
+            }
+          },
+          // {
+          //   $lookup: {
+          //     from: "evaluationFrameworks",
+          //     localField: "components.id",
+          //     foreignField: "_id",
+          //     as: "assessments"
+          //   }
+          // },
+          // { $unwind: "$assessments" },
+          {
+            $project: {
+              "programId": "$_id",
+              "schoolDocument._id": 1,
+              "schoolDocument.externalId": 1,
+              "schoolDocument.name": 1,
+              "_id": 0
+            }
+          }
+        ])
+
+        if (!programDocument) {
+          throw "Bad request"
+        }
+
+        return resolve({ message: "List of schools fetched successfully", result: programDocument })
+      }
+      catch (error) {
+        reject({
+          status: 400,
+          message: error
+        })
+      }
+    })
+  }
+
+  async listAssessors(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+
+        let programId = req.query.programId
+
+        if (!programId) {
+          throw "Program id is missing"
+        }
+
+        let componentId = req.query.componentId
+
+        if (!componentId) {
+          throw "Component id is missing"
+        }
+
+        let programDocument = await database.models.programs.aggregate([
+          {
+            $match: {
+              _id: ObjectId(programId)
+            }
+          }, {
+            $unwind: "$components"
+          }, {
+            $match: {
+              "components.id": ObjectId(componentId)
+            }
+          }, {
+            "$addFields": { "schoolIdInObjectIdForm": "$components.schools" }
+          },
+          {
+            $lookup: {
+              from: "schoolAssessors",
+              localField: "schoolIdInObjectIdForm",
+              foreignField: "schools",
+              as: "schoolInformation"
+            }
+          },
+          {
+            $project: {
+              "schoolInformation.role": 1,
+              "schoolInformation.programId": 1,
+              "schoolInformation.userId": 1,
+              "schoolInformation.programId": 1,
+              "schoolInformation.externalId": 1
+            }
+          }
+        ])
+
+        return resolve({
+          message: "Assessors fetched successfully",
+          result: programDocument
+        })
+
+      }
+      catch (error) {
+        return reject({
+          status: 400,
+          message: error
+        })
+      }
+    })
+  }
+
+  async assessorWiseSchools(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let assessorId = req.params._id
+
+        if (!assessorId) {
+          throw "Assessor id is missing"
+        }
+        let assessorWiseSchoolDocument = await database.models.schoolAssessors.aggregate([{
+          $match: {
+            _id: ObjectId(assessorId)
+          }
+        }, {
+          $lookup: {
+            from: "schools",
+            localField: "schools",
+            foreignField: "_id",
+            as: "assessorWiseSchools"
+          }
+        }, {
+          $project: {
+            externalId: 1,
+            role: 1,
+            programId: 1,
+            "assessorWiseSchools.externalId": 1,
+            "assessorWiseSchools.name": 1,
+            "assessorWiseSchools._id": 1
+          }
+        }
+        ])
+
+        if (!assessorWiseSchoolDocument) {
+          throw "Bad request"
+        }
+
+        resolve({ status: 200, message: "Assessor wise school information is fetched", result: assessorWiseSchoolDocument })
+      }
+      catch (error) {
+        return reject({
+          status: 400,
+          message: error
+        })
+      }
+    })
+  }
 };
