7380249237acd237234b3e0f5a199a7501a99f0d
ajithkumar <ajith@tunerlabs.com>
2019-02-11 11:52:57 +0530

    removed template for customised title, fav icon and so on...

--- public/apidoc-template/api_project.js
+++
@@ -1,1 +0,0 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2019-02-08T11:56:47.247Z",    "url": "http://apidocjs.com",    "version": "0.17.7"  }});
