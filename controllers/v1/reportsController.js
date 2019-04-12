1a5c43a013d71ea76511915b8ab7c044ac8b4aa7
aman-tunerlabs <aman@tunerlabs.com>
2019-04-12 09:56:00 +0530

    added condition for removing 26 criterias

--- controllers/v1/reportsController.js
+++ controllers/v1/reportsController.js
@@ -866,6 +866,9 @@ module.exports = class Reports {
 
         let arr ={}
 
+        let criteriasThatIsNotIncluded = ["CS/II/c1","CS/II/c2","CS/II/b1","CS/I/b1","TL/VI/b1","TL/VI/b2","TL/VI/b5","TL/VI/b6",
+        "TL/V/a1","TL/V/b1","TL/IV/b1","TL/IV/b2","TL/II/b2","TL/II/a1","TL/II/a2","TL/II/a3","TL/I/a4","TL/I/a5","SS/V/a3","SS/III/c2","SS/III/c1","SS/III/b1","SS/III/a1","SS/I/c3","SS/II/a1","SS/I/c2"]
+
         let getCriteriaPath =  function (themes,parentData = []) {
 
           themes.forEach(theme => {
@@ -911,9 +914,10 @@ module.exports = class Reports {
           });
         }());
 
+
         submissionDocument.criterias && submissionDocument.criterias.forEach(submissionCriterias => {
-    
-          if (submissionCriterias._id) {
+          
+          if (submissionCriterias._id && !criteriasThatIsNotIncluded.includes(submissionCriterias.externalId)) {
             let criteriaReportObject = {
               "Path To Criteria": arr[submissionCriterias._id.toString()] ? arr[submissionCriterias._id.toString()].parentPath : "",
               "Score": submissionCriterias.score
