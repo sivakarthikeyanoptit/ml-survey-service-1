a9d336ae933f455efdf18635a962496e922475c6
aks30 <aks301190@gmail.com>
2019-04-24 11:59:11 +0530

    Change upgrade and downgrade to up and down

--- module/migrations/samples/migration.js
+++ module/migrations/samples/migration.js
@@ -1,10 +1,10 @@
 module.exports = {
-  async upgrade(db) {
+  async up(db) {
     global.migrationMsg = "Include collectionName and what does it update"
     // return await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
   },
 
-  async downgrade(db) {
+  async down(db) {
     // return await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
   }
 };
