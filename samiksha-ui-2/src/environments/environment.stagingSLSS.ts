a74f3f2a7d8b6525137738957cf3b43c7fb74122
Kiran Haridas <kiran@Aboves-MacBook-Pro.local>
2019-05-09 14:38:48 +0530

    Environment change

--- samiksha-ui-2/src/environments/environment.stagingSLSS.ts
+++ samiksha-ui-2/src/environments/environment.stagingSLSS.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: "slss-assessment-service/web2/",
+  baseUrl: "slss-assessment-service/",
   apiHost: "https://staginghome.shikshalokam.org",
   apiBaseEndpoint: "/slss-assessment-service/web2/"
 };
