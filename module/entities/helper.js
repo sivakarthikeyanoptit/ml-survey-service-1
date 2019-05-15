a119185bb376b97998a60d7d30fad2f059db1998
ajithtunerlabs <ajith@tunerlabs.com>
2019-05-15 16:22:38 +0530

    added lean method and removed unused code

--- module/entities/helper.js
+++ module/entities/helper.js
@@ -7,7 +7,7 @@ module.exports = class entitiesHelper {
         return new Promise(async (resolve, reject) => {
             try {
 
-                let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { _id: 1 });
+                let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { _id: 1 }).lean();
 
                 if (!entityTypeDocument) throw "No entity type found for given params"
 
@@ -81,7 +81,7 @@ module.exports = class entitiesHelper {
         return new Promise(async (resolve, reject) => {
             try {
 
-                let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { profileForm: 1 })
+                let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { profileForm: 1 }).lean();
 
                 let result = entityTypeDocument.profileForm.length ? entityTypeDocument.profileForm : [];
 
@@ -114,8 +114,8 @@ module.exports = class entitiesHelper {
 
                 if (entityId) {
                     entityInformation = await database.models.entities.findOne(
-                        { _id: ObjectId(entityId), entityType: entityType }, { metaInformation: 1 }
-                    );
+                        { _id: ObjectId(entityId) }, { metaInformation: 1 }
+                    ).lean();
 
                     if (!entityInformation) {
                         throw `No ${entityType} information found for given params.`;
@@ -144,13 +144,13 @@ module.exports = class entitiesHelper {
                 let entityInformation;
 
                 if (entityType == "parent") {
-                    entityInformation = await this.parentRegistryUpdate(entityType, entityId, data);
+                    entityInformation = await this.parentRegistryUpdate(entityId, data);
                 } else {
                     entityInformation = await database.models.entities.findOneAndUpdate(
-                        { _id: ObjectId(entityId), entityType: entityType },
+                        { _id: ObjectId(entityId) },
                         { metaInformation: data },
                         { new: true }
-                    );
+                    ).lean();
                 }
 
                 return resolve(entityInformation);
@@ -161,13 +161,13 @@ module.exports = class entitiesHelper {
         })
     }
 
-    static parentRegistryUpdate(entityType, entityId, data) {
+    static parentRegistryUpdate(entityId, data) {
         return new Promise(async (resolve, reject) => {
             try {
 
                 const parentDocument = await database.models.entities.findOne(
-                    { _id: ObjectId(entityId), entityType: entityType }, { "metaInformation.callResponse": 1 }
-                );
+                    { _id: ObjectId(entityId) }, { "metaInformation.callResponse": 1 }
+                ).lean();
 
                 if (!parentDocument) throw "No such parent found"
 
@@ -185,7 +185,7 @@ module.exports = class entitiesHelper {
                     { _id: ObjectId(entityId) },
                     { metaInformation: data },
                     { new: true }
-                );
+                ).lean();
 
                 if (updateSubmissionDocument) {
 
@@ -196,7 +196,7 @@ module.exports = class entitiesHelper {
                     let submissionDocument = await database.models.submissions.findOne(
                         queryObject,
                         { ["parentInterviewResponses." + parentInformation._id.toString()]: 1, parentInterviewResponsesStatus: 1 }
-                    );
+                    ).lean();
 
                     let updateObject = {}
                     updateObject.$set = {}
@@ -237,7 +237,7 @@ module.exports = class entitiesHelper {
                     await database.models.submissions.findOneAndUpdate(
                         { _id: submissionDocument._id },
                         updateObject
-                    );
+                    ).lean();
                 }
 
                 return resolve(parentInformation);
@@ -250,151 +250,159 @@ module.exports = class entitiesHelper {
 
     static upload(entityType, programId, solutionId, userDetails, file) {
         return new Promise(async (resolve, reject) => {
-            let entityCSVData = await csv().fromString(
-                file.entities.data.toString()
-            );
-
-            let programIds = programId ? [programId] : entityCSVData.map(entity => entity.programId);
-
-            let programDocument = await database.models.programs.find(
-                {
-                    externalId: { $in: programIds }
-                },
-                {
-                    _id: 1,
-                    externalId: 1
-                }
-            ).lean();
-
-            const programsData = programDocument.reduce((ac, program) => ({ ...ac, [program.externalId]: program._id }), {})
-
-            let solutionIds = solutionId ? [solutionId] : entityCSVData.map(entity => entity.solutionId);
-
-            let solutionsDocument = await database.models.solutions.find(
-                {
-                    externalId: { $in: solutionIds }
-                },
-                {
-                    externalId: 1,
-                    subType: 1
-                }
-            ).lean();
-
-            const solutionsData = solutionsDocument.reduce((ac, solution) => ({
-                ...ac, [solution.externalId]: {
-                    subType: solution.subType,
-                    _id: solution._id
-                }
-            }), {});
-
-            let parentExternalIds = entityCSVData.map(entity => entity.parentEntityId);
-
-            let parentEntityDocument = await database.models.entities.find(
-                {
-                    "metaInformation.externalId": { $in: parentExternalIds }
-                },
-                {
-                    _id: 1,
-                    "metaInformation.externalId": 1
-                }
-            ).lean();
-
-            const parentEntityData = parentEntityDocument.reduce((ac, parentDocument) => ({ ...ac, [parentDocument.metaInformation.externalId]: parentDocument._id }), {})
-
-            let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { _id: 1 });
-
-            let entityDocuments = [];
-
-            entityCSVData.forEach(singleEntity => {
-
-                let arrayTypeFields = singleEntity.arrayFields.split(",")
-
-                Object.keys(singleEntity).forEach(data => {
-                    if (arrayTypeFields.includes(data)) {
-                        singleEntity[data] = singleEntity[data].split(",")
+            try {
+                let entityCSVData = await csv().fromString(
+                    file.entities.data.toString()
+                );
+    
+                let programIds = programId ? [programId] : entityCSVData.map(entity => entity.programId);
+    
+                let programDocument = await database.models.programs.find(
+                    {
+                        externalId: { $in: programIds }
+                    },
+                    {
+                        _id: 1,
+                        externalId: 1
                     }
-                })
-
-                singleEntity["createdByProgramId"] = programsData[singleEntity.programId]["_id"];
-
-                //parentEntityId needed to update parents entity
-                singleEntity["parentEntityId"] = parentEntityData[singleEntity.parentEntityId];
-
-                entityDocuments.push({
-                    "entityTypeId": entityTypeDocument._id,
-                    "entityType": entityType,
-                    "regsitryDetails": {},
-                    "groups": {},
-                    "metaInformation": singleEntity,
-                    "updatedBy": userDetails.id,
-                    "createdBy": userDetails.id
-                });
-
-            })
-
-            let entityData = await database.models.entities.create(
-                entityDocuments
-            );
-
-            //update entity id in solutions
-
-            let entityDataForSolutionsToBeMapped = entityData.filter(entity => entity.metaInformation.addEntityToSolution === "true")
-
-            if (entityDataForSolutionsToBeMapped.length) {
-                let groupedEntityDataBySolutionId = _.groupBy(entityDataForSolutionsToBeMapped, function (entityData) { return entityData.metaInformation.solutionId })
-
-                Object.keys(groupedEntityDataBySolutionId).forEach(async (solutionId) => {
-
-                    let entityIds = groupedEntityDataBySolutionId[solutionId].map(entity => entity._id);
-
-                    await database.models.solutions.updateOne({ _id: ObjectId(solutionsData[solutionId]._id) }, { $addToSet: { entities: { $each: entityIds } } })
-                })
-
-            }
-
-            //update entity id in parent entity
-
-            // await this.mapEntitiesToParentEntity(entityData);
-
-            entityCSVData.forEach(async (entity) => {
-
-                if (entity.createEntityAssessor && entity.createEntityAssessor === "true") {
-
-                    let entityDocument = entityData.find(entityDocument => {
-                        return (entityDocument.metaInformation.userId == entity.userId && entityDocument.metaInformation.solutionId == entity.solutionId)
+                ).lean();
+    
+                const programsData = programDocument.reduce((ac, program) => ({ ...ac, [program.externalId]: program._id }), {})
+    
+                let solutionIds = solutionId ? [solutionId] : entityCSVData.map(entity => entity.solutionId);
+    
+                let solutionsDocument = await database.models.solutions.find(
+                    {
+                        externalId: { $in: solutionIds }
+                    },
+                    {
+                        externalId: 1,
+                        subType: 1
+                    }
+                ).lean();
+    
+                const solutionsData = solutionsDocument.reduce((ac, solution) => ({
+                    ...ac, [solution.externalId]: {
+                        subType: solution.subType,
+                        _id: solution._id
+                    }
+                }), {});
+    
+                // let parentExternalIds = entityCSVData.map(entity => entity.parentEntityId);
+    
+                // let parentEntityDocument = await database.models.entities.find(
+                //     {
+                //         "metaInformation.externalId": { $in: parentExternalIds }
+                //     },
+                //     {
+                //         _id: 1,
+                //         "metaInformation.externalId": 1
+                //     }
+                // ).lean();
+    
+                // const parentEntityData = parentEntityDocument.reduce((ac, parentDocument) => ({ ...ac, [parentDocument.metaInformation.externalId]: parentDocument._id }), {})
+    
+                let entityTypeDocument = await database.models.entityTypes.findOne({ name: entityType }, { _id: 1 });
+    
+                let entityDocuments = [];
+    
+                entityCSVData.forEach(singleEntity => {
+    
+                    let arrayTypeFields = singleEntity.arrayFields.split(",")
+    
+                    Object.keys(singleEntity).forEach(data => {
+                        if (arrayTypeFields.includes(data)) {
+                            singleEntity[data] = singleEntity[data].split(",")
+                        }
                     })
-
-                    if (entityDocument) {
-                        // createEntityAssessor(programId, solutionId, entityId, entity, userDetails) - for understanding purpose.
-                        entity.role = "ASSESSOR";
-                        await entityAssessorsHelper.createEntityAssessor(programsData[entity.programId], solutionsData[entity.solutionId]._id, entityDocument._id, entity, userDetails);
-                    }
-
+    
+                    singleEntity["createdByProgramId"] = programsData[singleEntity.programId]["_id"];
+    
+                    //parentEntityId needed to update parents entity
+                    // singleEntity["parentEntityId"] = parentEntityData[singleEntity.parentEntityId];
+    
+                    entityDocuments.push({
+                        "entityTypeId": entityTypeDocument._id,
+                        "entityType": entityType,
+                        "regsitryDetails": {},
+                        "groups": {},
+                        "metaInformation": singleEntity,
+                        "updatedBy": userDetails.id,
+                        "createdBy": userDetails.id
+                    });
+    
+                })
+    
+                let entityData = await database.models.entities.create(
+                    entityDocuments
+                );
+    
+                //update entity id in solutions
+    
+                let entityDataForSolutionsToBeMapped = entityData.filter(entity => entity.metaInformation.addEntityToSolution === "true")
+    
+                if (entityDataForSolutionsToBeMapped.length) {
+                    let groupedEntityDataBySolutionId = _.groupBy(entityDataForSolutionsToBeMapped, function (entityData) { return entityData.metaInformation.solutionId })
+    
+                    Object.keys(groupedEntityDataBySolutionId).forEach(async (solutionId) => {
+    
+                        let entityIds = groupedEntityDataBySolutionId[solutionId].map(entity => entity._id);
+    
+                        await database.models.solutions.updateOne({ _id: ObjectId(solutionsData[solutionId]._id) }, { $addToSet: { entities: { $each: entityIds } } })
+                    })
+    
                 }
-
-            })
-            return resolve();
+    
+                //update entity id in parent entity
+    
+                // await this.mapEntitiesToParentEntity(entityData);
+    
+                entityCSVData.forEach(async (entity) => {
+    
+                    if (entity.createEntityAssessor && entity.createEntityAssessor === "true") {
+    
+                        let entityDocument = entityData.find(entityDocument => {
+                            return (entityDocument.metaInformation.userId == entity.userId && entityDocument.metaInformation.solutionId == entity.solutionId)
+                        })
+    
+                        if (entityDocument) {
+                            // createEntityAssessor(programId, solutionId, entityId, entity, userDetails) - for understanding purpose.
+                            entity.role = "ASSESSOR";
+                            await entityAssessorsHelper.createEntityAssessor(programsData[entity.programId], solutionsData[entity.solutionId]._id, entityDocument._id, entity, userDetails);
+                        }
+    
+                    }
+    
+                })
+                return resolve();
+            } catch (error) {
+                return reject(error)
+            }
         })
 
     }
 
-    static mapEntitiesToParentEntity(entityData, entityType, programId) {
+    static mapEntitiesToParentEntity(entityData, entityType) {
         return new Promise(async (resolve, reject) => {
-            await Promise.all(entityData.map(async (entity) => {
-
-                await database.models.entities.findOneAndUpdate(
-                    {
-                        _id: ObjectId(entity.metaInformation.parentEntityId),
-                        "metaInformation.createdByProgramId": entity.metaInformation.createdByProgramId
-                    },
-                    {
-                        $addToSet: {
-                            [`groups.${entityType}`]: entity._id
-                        }
-                    });
-
-            }))
-            return resolve();
+            try {
+                await Promise.all(entityData.map(async (entity) => {
+    
+                    await database.models.entities.findOneAndUpdate(
+                        {
+                            _id: ObjectId(entity.metaInformation.parentEntityId),
+                            "metaInformation.createdByProgramId": entity.metaInformation.createdByProgramId
+                        },
+                        {
+                            $addToSet: {
+                                [`groups.${entityType}`]: entity._id
+                            }
+                        });
+    
+                }))
+                return resolve();
+            } catch (error) {
+                return reject(error)
+            }
         })
     }
 
--- module/entityAssessor/helper.js
+++ module/entityAssessor/helper.js
@@ -1,21 +1,22 @@
-const csv = require("csvtojson");
-
 module.exports = class entityAssessorHelper {
 
-    static createEntityAssessor(programId,solutionId,entityId,userEntityDetails,userDetails) {
-        return new Promise(async(resolve,reject)=>{
-            userEntityDetails.programId = programId;
-            userEntityDetails.assessmentStatus = "pending";
-            userEntityDetails.parentId = "";
-            userEntityDetails["entities"] = entityId;
-            userEntityDetails.solutionId = solutionId;
-            userEntityDetails.createdBy = userDetails.id;
-            userEntityDetails.updatedBy = userDetails.id;
-            let entityAssessor = await database.models.entityAssessors.create(
-                userEntityDetails
-            );
-    
-            return resolve(entityAssessor);
+    static createEntityAssessor(programId, solutionId, entityId, userEntityDetails, userDetails) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                userEntityDetails.programId = programId;
+                userEntityDetails.assessmentStatus = "pending";
+                userEntityDetails.parentId = "";
+                userEntityDetails["entities"] = entityId;
+                userEntityDetails.solutionId = solutionId;
+                userEntityDetails.createdBy = userDetails.id;
+                userEntityDetails.updatedBy = userDetails.id;
+                let entityAssessor = await database.models.entityAssessors.create(
+                    userEntityDetails
+                );
+                return resolve(entityAssessor);
+            } catch (error) {
+                return reject(error)
+            }
         })
     }
 
