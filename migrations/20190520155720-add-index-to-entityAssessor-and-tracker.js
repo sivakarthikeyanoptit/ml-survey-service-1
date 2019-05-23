2a3b0de11788e28530985487ca0292d150cd506c
ajithtunerlabs <ajith@tunerlabs.com>
2019-05-23 14:14:53 +0530

    added program and solution index for entity assessor and tracker

--- migrations/20190520155720-add-index-to-entityAssessor-and-tracker.js
+++ migrations/20190520155720-add-index-to-entityAssessor-and-tracker.js
@@ -2,7 +2,11 @@ module.exports = {
   async up(db) {
 
     await db.collection('entityAssessors').createIndex( { userId: 1} )
-    await db.collection('entityAssessorsTrackers').createIndex( { programId: 1, assessorUserId:1} )
+    await db.collection('entityAssessors').createIndex( { programId: 1} )
+    await db.collection('entityAssessors').createIndex( { solutionId: 1} )
+    await db.collection('entityAssessorsTrackers').createIndex( { assessorUserId:1} )
+    await db.collection('entityAssessorsTrackers').createIndex( { programId: 1} )
+    await db.collection('entityAssessorsTrackers').createIndex( { solutionId:1} )
 
   },
 
