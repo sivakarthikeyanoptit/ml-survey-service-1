3531ff566da05b2ba2da88c01071101292d38a00
ajithtunerlabs <ajith@tunerlabs.com>
2019-05-24 14:12:44 +0530

    removed unused code

--- generics/middleware/userPrograms.js
+++ generics/middleware/userPrograms.js
@@ -1,13 +1,6 @@
 module.exports = async (req, res, next) => {
     if (req.userDetails && req.userDetails.id) {
 
-        let roles = _.pull(req.userDetails.allRoles, 'PUBLIC');
-        let queryParams = roles.map(role => {
-            return {
-                [`roles.${gen.utils.mapUserRole(role)}.users`]: { $in: [req.userDetails.id] }
-            }
-        })
-
         let entityAssessorDocumentByUser = await database.models.entityAssessors.find({userId:req.userDetails.id},{solutionId:1}).lean();
 
         let solutions = await database.models.solutions.find({ "_id": {$in: entityAssessorDocumentByUser.map(solution=>solution.solutionId)} }, {
