e163f53926f38d0f115354758a183f9210e934e7
aman-tunerlabs <aman@tunerlabs.com>
2019-08-22 11:19:28 +0530

    adding v1 and v2 inside validator folders

--- generics/middleware/validator/index.js
+++ generics/middleware/validator/index.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 module.exports = (req, res, next) => {
 
-    let validatorPath = ROOT_PATH + `/module/${req.params.controller}/${req.params.version}.js`
+    let validatorPath = ROOT_PATH + `/module/${req.params.controller}/validator/${req.params.version}.js`
 
     if (fs.existsSync(validatorPath)) require(validatorPath)(req);
 
