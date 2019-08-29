aa1d06d23c09bd0fff99770702a0f1ef7f0c261b
Akash Shah <akash.shah@above-inc.com>
2019-08-29 22:37:53 +0530

    API to createGroupEntityTypeIndex.

--- controllers/v1/entityTypesController.js
+++ controllers/v1/entityTypesController.js
@@ -1,4 +1,5 @@
 const entitiyTypesHelper = require(ROOT_PATH + "/module/entityTypes/helper")
+const entitiesHelper = require(ROOT_PATH + "/module/entities/helper")
 
 module.exports = class EntityTypes extends Abstract {
   constructor() {
@@ -21,7 +22,7 @@ module.exports = class EntityTypes extends Abstract {
   * @apiUse errorBody
   */
 
-  canBeObserved(req) {
+  canBeObserved() {
     return new Promise(async (resolve, reject) => {
 
       try {
@@ -31,6 +32,57 @@ module.exports = class EntityTypes extends Abstract {
         return resolve({
           message: "Entity type fetched successfully.",
           result: result
+        });
+
+      } catch (error) {
+
+        return reject({
+          status: error.status || 500,
+          message: error.message || "Oops! something went wrong.",
+          errorObject: error
+        })
+
+      }
+
+
+    })
+  }
+
+
+  /**
+  * @api {get} /assessment/api/v1/entityTypes/createGroupEntityTypeIndex Create groups.entityType index
+  * @apiVersion 0.0.1
+  * @apiName Create groups.entityType index
+  * @apiGroup Entity Types
+  * @apiHeader {String} X-authenticated-user-token Authenticity token
+  * @apiSampleRequest /assessment/api/v1/entityTypes/createGroupEntityTypeIndex
+  * @apiUse successBody
+  * @apiUse errorBody
+  */
+
+  createGroupEntityTypeIndex() {
+    return new Promise(async (resolve, reject) => {
+
+      try {
+
+        const result = await entitiyTypesHelper.list({},{name :1});
+        
+        let indexResult = await Promise.all(result.map(async entityType => {
+          const indexCreation = await entitiesHelper.createGroupEntityTypeIndex(entityType.name)
+          return "Index successfully created for entity type - "+entityType.name
+        }))
+
+        if (
+          indexResult.findIndex(
+            index => index === undefined || index === null
+          ) >= 0
+        ) {
+          throw "Something went wrong, entity group index was not created.";
+        }
+
+        return resolve({
+          message: "Entity type index created successfully.",
+          result: indexResult
         });
 
       } catch (error) {
