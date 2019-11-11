aa6563e598d1b5291c289bc788fdedc66700b27e
aman-tunerlabs <aman@tunerlabs.com>
2019-11-11 12:39:23 +0530

    Basic api created for pending observations and pending assessments

--- controllers/v1/entityAssessorsController.js
+++ controllers/v1/entityAssessorsController.js
@@ -281,4 +281,137 @@ module.exports = class EntityAssessors extends Abstract {
     })
   }
 
+  /**
+* @api {get} /assessment/api/v1/entityAssessors/pendingAssessments/{{userId}}?assessorExternalId=:assessorExternalId Pending Assessments
+* @apiVersion 1.0.0
+* @apiName Pending Assessments
+* @apiGroup Entity Assessor
+* @apiParam {String} assessorExternalId Required Assessor External Id.
+* @apiHeader {String} X-authenticated-user-token Authenticity token
+* @apiSampleRequest /assessment/api/v1/entityAssessors/pendingAssessments/e97b5582-471c-4649-8401-3cc4249359bb?assessorExternalId=a1
+* @apiUse successBody
+* @apiUse errorBody
+* @apiParamExample {json} Response:
+*/
+
+  async pendingAssessments(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+
+        let entityAssessorsDocument = await database.models.entityAssessors.findOne({
+          userId: req.params._id,
+          externalId: req.query.assessorExternalId,
+          role: { $in: ["ASSESSOR", "LEAD_ASSESSOR"] },
+        }, { solutionId: 1, entityTypeId: 1, entities: 1, programId: 1 }).lean();
+
+        if (!entityAssessorsDocument) {
+          throw { message: "You are not a LEAD_ASSESSOR or ASSESSOR" }
+        }
+
+        let queryObj = {
+          entityTypeId: entityAssessorsDocument.entityTypeId,
+          solutionId: entityAssessorsDocument.solutionId,
+          status: { $ne: "completed" },
+          programId: entityAssessorsDocument.programId,
+          entityId: { $in: entityAssessorsDocument.entities }
+        }
+
+        let projection = { createdAt: 1, entityExternalId: 1, programExternalId: 1 }
+
+        let assessmentSubmissions = await database.models.submissions.find(queryObj, projection).lean()
+
+        let response = [];
+
+        if (assessmentSubmissions.length > 0) {
+          assessmentSubmissions.map(eachAssessment => {
+            eachAssessment["type"] = "assessment"
+            eachAssessment["status"] = "Pending"
+            return eachAssessment
+          })
+
+          response = _.merge(response, assessmentSubmissions)
+        }
+
+        return resolve({
+          message: "Pending Assessments",
+          result: response
+        });
+
+      } catch (error) {
+        return reject({
+          status: error.status || 500,
+          message: error.message || "Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
+
+  /**
+* @api {get} /assessment/api/v1/entityAssessors/pendingObservations/{{userId}}?assessorExternalId=:assessorExternalId Pending Observations
+* @apiVersion 1.0.0
+* @apiName Pending Observations
+* @apiGroup Entity Assessor
+* @apiParam {String} assessorExternalId required Assessor External Id.
+* @apiHeader {String} X-authenticated-user-token Authenticity token
+* @apiSampleRequest /assessment/api/v1/entityAssessors/pendingObservations/e97b5582-471c-4649-8401-3cc4249359bb?assessorExternalId=a1
+* @apiUse successBody
+* @apiUse errorBody
+* @apiParamExample {json} Response:
+*/
+
+  async pendingObservations(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+
+        let entityAssessorsDocument = await database.models.entityAssessors.findOne({
+          userId: req.params._id,
+          externalId: req.query.assessorExternalId,
+          role: { $in: ["ASSESSOR", "LEAD_ASSESSOR"] }
+        }, { solutionId: 1, entityTypeId: 1, entities: 1, programId: 1 }).lean();
+
+        if (!entityAssessorsDocument) {
+          throw { message: "You are not a LEAD_ASSESSOR or ASSESSOR" }
+        }
+
+        let queryObj = {
+          entityTypeId: entityAssessorsDocument.entityTypeId,
+          solutionId: entityAssessorsDocument.solutionId,
+          status: { $ne: "completed" },
+          programId: entityAssessorsDocument.programId,
+          entityId: { $in: entityAssessorsDocument.entities }
+        }
+
+        let projection = { createdAt: 1, entityExternalId: 1, programExternalId: 1 }
+
+        let observationSubmissions = await database.models.observationSubmissions.find(queryObj, projection)
+
+        let response = [];
+
+        if (observationSubmissions.length > 0) {
+
+          observationSubmissions.map(eachObservationSubmissions => {
+            eachObservationSubmissions["type"] = "observation"
+            eachObservationSubmissions["status"] = "Pending"
+            return eachObservationSubmissions
+          })
+
+          response = _.merge(response, observationSubmissions)
+        }
+
+        return resolve({
+          message: "Pending Observations",
+          result: response
+        });
+      } catch (error) {
+        return reject({
+          status: error.status || 500,
+          message: error.message || "Oops! Something went wrong!",
+          errorObject: error
+        });
+      }
+    });
+  }
+
+
 };
