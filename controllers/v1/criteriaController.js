8da21401eaa2ba7a8b59ef4dab876d76c4855735
aman-tunerlabs <45452751+aman-tunerlabs@users.noreply.github.com>
2020-01-23 12:32:48 +0530

    STORY#267 - TASK#1835 created criteria,frameworks,questions,solutions creation api (#529)

--- controllers/v1/criteriaController.js
+++ controllers/v1/criteriaController.js
@@ -8,6 +8,7 @@
 // Dependencies
 const csv = require("csvtojson");
 const FileStream = require(ROOT_PATH + "/generics/fileStream");
+const criteriasHelper = require(MODULES_BASE_PATH + "/criteria/helper");
 
  /**
     * Criteria
@@ -198,6 +199,101 @@ module.exports = class Criteria extends Abstract {
       }
     })
   }
+
+  /**
+   * @api {post} /assessment/api/v1/criteria/create Create Questions 
+   * @apiVersion 1.0.0
+   * @apiName Create criteria
+   * @apiGroup Criteria
+   * @apiUse successBody
+   * @apiUse errorBody
+   */
+
+  /**
+   * create criteria.
+   * @method
+   * @name create
+   * @param {Object} req - requested data.
+   * @param {Object} req.body - requested body data. 
+   * @returns {Object} 
+   */
+
+  create(req){
+    return new Promise(async (resolve, reject) => {
+      try {
+        let criteriaDocuments = await criteriasHelper.create(req.body);
+        return resolve({
+          result : criteriaDocuments
+        });
+
+      } catch (error) {
+        reject({
+          message: error
+        });
+      }
+    });
+  }
+
+    /**
+   * @api {post} /assessment/api/v1/criteria/update Create Questions 
+   * @apiVersion 1.0.0
+   * @apiName Update criteria
+   * @apiGroup Criteria
+   * @apiUse successBody
+   * @apiUse errorBody
+   */
+
+  /**
+   * Update criteria.
+   * @method
+   * @name create
+   * @param {Object} req - requested data.
+   * @param {Object} req.body - requested criteria update data. 
+   * @returns {Object} 
+   */
+
+  async update(req) {
+    return new Promise(async (resolve, reject) => {
+      try {
+
+        let queryObject = {
+          externalId: req.query.externalId
+        }
+
+        if(req.query.frameworkIdExists) {
+          queryObject["frameworkCriteriaId"] = {
+            $exists : true
+          }
+        };
+
+        let updateObject = {
+          "$set" : {}
+        };
+
+        let criteriaUpdateData = req.body;
+
+        Object.keys(criteriaUpdateData).forEach(criteriaData=>{
+          updateObject["$set"][criteriaData] = criteriaUpdateData[criteriaData];
+        })
+
+        updateObject["$set"]["updatedBy"] = req.userDetails.id;
+
+        await database.models.criteria.findOneAndUpdate(queryObject, updateObject)
+
+        return resolve({
+          status: httpStatusCode.ok.status,
+          message: messageConstants.apiResponses.CRITERIA_UPDATED
+        });
+      }
+      catch (error) {
+        reject({
+          status: error.status || httpStatusCode.internal_server_error.status,
+          message: error.message || httpStatusCode.internal_server_error.message,
+          errorObject: error
+        })
+      }
+    })
+  } 
 
 };
 
