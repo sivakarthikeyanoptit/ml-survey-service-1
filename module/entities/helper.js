328c0a30236e89dadfb31db025e07a66ba8e1fdd
Akash Shah <aks301190@gmail.com>
2020-03-11 14:39:35 +0530

    Temp commit.

--- module/entities/helper.js
+++ module/entities/helper.js
@@ -714,8 +714,7 @@ module.exports = class EntitiesHelper {
                             name: "$metaInformation.name",
                             externalId: "$metaInformation.externalId",
                             addressLine1: "$metaInformation.addressLine1",
-                            addressLine2: "$metaInformation.addressLine2",
-                            districtName: "$metaInformation.districtName"
+                            addressLine2: "$metaInformation.addressLine2"
                         }
                     },
                     {
