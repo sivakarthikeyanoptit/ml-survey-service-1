70ab1dcff5a63615a40145003928ccd63a876361
Akash Shah <aks301190@gmail.com>
2020-04-24 22:21:36 +0530

    Put criteria scoring and rating logic in a common scoring helper.

--- module/observationSubmissions/helper.js
+++ module/observationSubmissions/helper.js
@@ -10,7 +10,7 @@
 let slackClient = require(ROOT_PATH + "/generics/helpers/slackCommunications");
 let kafkaClient = require(ROOT_PATH + "/generics/helpers/kafkaCommunications");
 const emailClient = require(ROOT_PATH + "/generics/helpers/emailCommunications");
-const submissionsHelper = require(MODULES_BASE_PATH + "/submissions/helper")
+const scoringHelper = require(MODULES_BASE_PATH + "/scoring/helper")
 const criteriaHelper = require(MODULES_BASE_PATH + "/criteria/helper")
 const questionsHelper = require(MODULES_BASE_PATH + "/questions/helper")
 
@@ -277,7 +277,7 @@ module.exports = class ObservationSubmissionsHelper {
                 })
                 }
 
-                let resultingArray = await submissionsHelper.rateEntities([submissionDocument], "singleRateApi");
+                let resultingArray = await scoringHelper.rateEntities([submissionDocument], "singleRateApi");
 
                 if(resultingArray.result.runUpdateQuery) {
                     await database.models.observationSubmissions.updateOne(
