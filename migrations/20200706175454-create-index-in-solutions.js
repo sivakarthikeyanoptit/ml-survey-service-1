f3a03f8a07da434b13da5c291a8f0f6d0399c0a5
Akash Shah <aks301190@gmail.com>
2020-07-07 20:58:03 +0530

    Update 20200706175454-create-index-in-solutions.js

--- migrations/20200706175454-create-index-in-solutions.js
+++ migrations/20200706175454-create-index-in-solutions.js
@@ -2,11 +2,11 @@ module.exports = {
   async up(db) {
     global.migrationMsg = "create indexes of name,description,status,isReusable,keywords in solutions";
     await db.collection('solutions').createIndex({ isReusable : 1 });
+    await db.collection('solutions').createIndex({ status : 1 });
     await db.collection('solutions').createIndex({ 
       name : "text",
       description : "text",
-      keywords : "text",
-      status : "text"
+      keywords : "text"
     },{ 
       language_override: "dummy",
       weights : {
