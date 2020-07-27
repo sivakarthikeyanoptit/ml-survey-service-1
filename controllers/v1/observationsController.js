0dcf6e4fa1a8af3e4db04bacb93fbf4b0eda26c5
aman-tunerlabs <aman@tunerlabs.com>
2020-07-27 19:41:03 +0530

    Added program data filter in bulk observation creation and program data check in importFromSolution apis.

--- controllers/v1/observationsController.js
+++ controllers/v1/observationsController.js
@@ -1167,7 +1167,7 @@ module.exports = class Observations extends Abstract {
                         templateId : newBaseSolution._id
                     };
 
-                    if( programDocument[0]._id ) {
+                    if( programDocument && programDocument[0]._id ) {
 
                         newSolutionDocument["programId"] = programDocument[0]._id;
                         newSolutionDocument["programName"] = programDocument[0].name;
@@ -1344,7 +1344,8 @@ module.exports = class Observations extends Abstract {
                     status: "active",
                     isDeleted: false,
                     isReusable: false,
-                    type: "observation"
+                    type: "observation",
+                    programId : { $exists : true }
                 };
 
                 let solutionProjection = [
