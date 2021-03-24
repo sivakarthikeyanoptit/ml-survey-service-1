1f970ca8205fc257f056628ad2ce78d344a7e3ac
Akash Shah <aks301190@gmail.com>
2021-03-24 16:24:35 +0530

    Put logs.

--- generics/kafkaConsumers/submissionRatingQueueConsumer.js
+++ generics/kafkaConsumers/submissionRatingQueueConsumer.js
@@ -8,16 +8,34 @@ var messageReceived = function (message) {
     try {
 
         let parsedMessage = JSON.parse(message.value)
-
+      
+        // Parsed Message Strucutre
+        //   {
+        //     submissionModel : "observationSubmissions/submissions",
+        //     submissionId : observationSubmissionId
+        //   }
+        console.log("############################ AUTO RATING LOGS STARTS ############################")
+        console.log(parsedMessage)
+        console.log("############################ AUTO RATING LOGS ENDS ############################")
         if(parsedMessage.submissionModel == "observationSubmissions") {
+            console.log("GOING TO RATE OBSERVATION SUBMISSIONS")
+            console.log(parsedMessage.submissionId)
             const observationSubmissionRatingResponse = await observationSubmissionsHelper.rateSubmissionById(parsedMessage.submissionId)
+            console.log(observationSubmissionRatingResponse)
         } else if (parsedMessage.submissionModel == "submissions") {
+            console.log("GOING TO RATE  SUBMISSIONS")
+            console.log(parsedMessage.submissionId)
             const submissionRatingResponse = await submissionsHelper.rateSubmissionById(parsedMessage.submissionId)
+            console.log(submissionRatingResponse)
         }
-        
+
+        console.log("Message Processed.")
+        console.log("############################ AUTO RATING LOGS ENDS ############################")
         return resolve("Message Processed.");
 
     } catch (error) {
+      console.log(error)
+      console.log("############################ AUTO RATING LOGS ENDS ############################")
         return reject(error);
     }
 
