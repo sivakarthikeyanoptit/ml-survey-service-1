786c5f41a79f19b6a93f2f0e9460bb8ede971129
deepakr123 <deepa.kr@tibilsolutions.com>
2021-03-24 11:15:29 +0530

    added logs

--- generics/kafkaConsumers/submissionRatingQueueConsumer.js
+++ generics/kafkaConsumers/submissionRatingQueueConsumer.js
@@ -6,7 +6,8 @@ var messageReceived = function (message) {
   return new Promise(async function (resolve, reject) {
 
     try {
-
+        console.log("############################ OBSERVATION SUBMISSIONS HELPER ####################")
+        console.log("observationSubmissionsHelper");
         let parsedMessage = JSON.parse(message.value)
       
         // Parsed Message Strucutre
@@ -18,14 +19,17 @@ var messageReceived = function (message) {
         console.log(parsedMessage)
         console.log("############################ AUTO RATING LOGS ENDS ############################")
         if(parsedMessage.submissionModel == "observationSubmissions") {
+            console.log("###################### coming inside observationSubmissions ###########");
             const observationSubmissionRatingResponse = await observationSubmissionsHelper.rateSubmissionById(parsedMessage.submissionId)
         } else if (parsedMessage.submissionModel == "submissions") {
+            console.log("#####################coming inside submissions ############################");
             const submissionRatingResponse = await submissionsHelper.rateSubmissionById(parsedMessage.submissionId)
         }
-
+        console.log("message processed");
         return resolve("Message Processed.");
 
     } catch (error) {
+        console.log("error ", error);
         return reject(error);
     }
 
