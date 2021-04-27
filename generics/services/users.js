d907cb61bc218cafaddfee7d2253a9548350a14f
aman-tunerlabs <aman@tunerlabs.com>
2021-04-27 11:12:55 +0530

    removed authorization from user profile api

--- generics/services/users.js
+++ generics/services/users.js
@@ -22,7 +22,6 @@ const profile = function ( token,userId = "" ) {
             const options = {
                 headers : {
                     "content-type": "application/json",
-                    AUTHORIZATION : process.env.AUTHORIZATION,
                     "internal-access-token": process.env.INTERNAL_ACCESS_TOKEN,
                     "x-authenticated-user-token" : token
                 }
